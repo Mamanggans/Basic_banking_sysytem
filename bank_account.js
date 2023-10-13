4d3c15fc39b30fce407ef6c669d7ae30e3bd57fc
ferdiansyah <ferdikings03@gmail.com>
2023-10-13 13:52:57 +0700

    komit challenge kedua

---
+++ bank_account.js
@@ -0,0 +1,52 @@
+class BankAccount {
+    constructor() {
+      this.saldo = 0;
+    }
+    updateSaldo() {
+      const saldoElement = document.getElementById("saldo");
+      if (saldoElement) {
+        saldoElement.textContent = this.saldo;
+      }
+    }
+    tambahSaldo() {
+        while (true) {
+            var tambahan = parseFloat(prompt("Masukkan jumlah saldo yang ingin ditambahkan: "));
+            if (!isNaN(tambahan)&& tambahan >= 0) {
+                this.saldo += tambahan;
+                this.updateSaldo();
+                var lanjutkan = window.confirm(`Sisa saldo anda sekarang adalah: ${this.saldo}. Apakah ingin menambahkan? Klik "Cancel" jika tidak ingin lanjut.`);
+                if (!lanjutkan) {
+                    break;
+                }
+            } else if (isNaN(tambahan)){
+                alert("Input tidak valid. Masukkan angka yang valid."); 
+            } else if (tambahan <= 0){
+                alert ("masukin lebih dari 0");
+            }
+        }
+    }
+    kurangiSaldo() {
+        while (true) {
+            var pengurangan = parseFloat(prompt("Masukkan jumlah saldo yang ingin dikurangkan: "));
+            if (!isNaN(pengurangan) && pengurangan >= 0) {
+                if (pengurangan <= saldo) {
+                    this.saldo -= pengurangan;
+                    this.updateSaldo();
+                    var lanjutkan = window.confirm(`Sisa saldo anda sekarang adalah: ${this.saldo}. Apakah ingin mengurangkan? Klik "Cancel" jika tidak ingin lanjut.`);
+                    if (!lanjutkan) {
+                        break;
+                    }
+                } else {
+                    alert("Jumlah yang ingin Anda kurangkan melebihi saldo yang tersedia.");
+                }
+            } else if (isNaN(tambahan)){
+                alert("Input tidak valid. Masukkan angka yang valid."); 
+            } else {
+                alert ("masukin lebih dari 0");
+            }
+        }
+    }
+}
+
+const bankAccount = new BankAccount();
+bankAccount.updateSaldo(); 
